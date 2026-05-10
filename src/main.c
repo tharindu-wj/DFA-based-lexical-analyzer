e6a38f6bb57b3fa26c54dc668e2edb1fb139b201
Tharindu Wickramasinghe <tharinduwjh@gmail.com>
2026-05-11 00:30:10 +0930

    file output error

--- src/main.c
+++ src/main.c
@@ -20,22 +20,21 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    FILE *input  = fopen(argv[1], "r");
-    if (!input ) {
+    FILE *input = fopen(argv[1], "r");
+    if (!input) {
         printf("Error: could not open file '%s'.\n", argv[1]);
         return 1;
     }
 
-    FILE *output = NULL;
-    if (argc >= 3) {
-        output = fopen(argv[2], "w");
-        if (output == NULL) {
-            printf("Error: could not open '%s'\n", argv[2]);
-            fclose(input);
-            return 1;
-        }
-        logger_set_output_file(output);
+    const char *output_path = (argc >= 3) ? argv[2] : "tokens.out";
+    FILE *output = fopen(output_path, "w");
+    if (output == NULL) {
+        printf("Error: could not open '%s'\n", argv[2]);
+        fclose(input);
+        return 1;
     }
+    logger_set_output_file(output);
+
 
     int result = run_lexer(input);
 
